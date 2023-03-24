dfcd4c47ee1cf5e10973b68940d40df35339cf27
Nick Pisciotta <npisciotta@gmail.com>
2023-03-24 18:34:37 -0400

    Update configuration for team six and switch from sandbox env to nonprod

--- versions.tf
+++ versions.tf
@@ -10,7 +10,7 @@ terraform {
     hostname     = "app.terraform.io"
     organization = "twdps"
     workspaces {
-      prefix = "lab-platform-vpc-"
+      prefix = "six-lab-platform-vpc-"
     }
   }
 }
@@ -19,14 +19,14 @@ provider "aws" {
   region  = var.aws_region
   assume_role {
     role_arn     = "arn:aws:iam::${var.account_id}:role/${var.assume_role}"
-    session_name = "lab-platform-vpc-${var.cluster_name}"
+    session_name = "six-lab-platform-vpc-${var.cluster_name}"
   }
 
   default_tags {
     tags = {
       env      = var.cluster_name
       cluster  = var.cluster_name
-      pipeline = "lab-platform-vpc"
+      pipeline = "six-lab-platform-vpc"
       "kubernetes.io/cluster/${var.cluster_name}" = "shared"
     }
   }
